6f1f04fe84a6dbb1482148d9ebae512b27a7f6cd
Valery-Dm <dmitriev.valery@gmail.com>
2015-02-17 19:57:52 +0300

    add notes

--- html/index.php
+++ html/index.php
@@ -8,6 +8,9 @@
 	</head>
 	<body>
 		<h1>Project 0</h1>
+		<h2>Git commands</h2>
+		<h3>git config --global credential.helper cache</h3>
+		<p>store password in cache for 15 minutes (default)</p>
 		
 	</body>
 </html>
